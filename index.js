470eb5483df824e7e2d874b0319a81b211b1e857
Michael Fursa <bellamy9678@yahoo.com>
2020-06-08 13:27:55 +0300

    feat: singleton

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hola');
+import { Singleton } from './singleton.js';
+
+const single1 = new Singleton();
+const single2 = new Singleton();
+
+console.log(single1, single2);
+console.log(single1 === single2);
